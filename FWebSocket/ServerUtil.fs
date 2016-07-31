409eedabb0f91c055819d4cc187c32f7681ee2a6
Timofey Andriyaschenko <Timofey Andriyaschenko>
2016-08-01 01:58:25 +0300

    Added ability to receive data

---
+++ FWebSocket/ServerUtil.fs
@@ -0,0 +1,70 @@
+﻿namespace WebSocket
+
+open System
+open System.IO
+open System.Text
+open System.Security.Cryptography
+
+type OpCode = Text=0x1uy | Pong=0xAuy
+
+type Msg =
+    | Connect of MailboxProcessor<byte array>
+    | Disconnect of MailboxProcessor<byte array>
+    | Data of byte array
+
+module ServerUtil =
+
+  let pingInterval = 3000
+
+  let guid6455 = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+
+  /// headers can occur in any order (See 1.3 in [1])
+  let isWebSocketsUpgrade (lines: string array) = 
+    [| "GET /echo HTTP/1.1"; "Upgrade: websocket"; "Connection: Upgrade"|] 
+    |> Array.map(
+        fun x->lines |> Array.exists(fun y->x.ToLower()=y.ToLower()))
+    |> Array.reduce(fun x y->x && y)
+  
+  /// see: [1]
+  let calcWSAccept6455 (secWebSocketKey:string) = 
+    let ck = secWebSocketKey + guid6455
+    let sha1 = SHA1CryptoServiceProvider.Create()
+    let hashBytes = ck |> Encoding.ASCII.GetBytes |> sha1.ComputeHash
+    let Sec_WebSocket_Accept = hashBytes |> Convert.ToBase64String
+    Sec_WebSocket_Accept
+
+  let createAcceptString6455 acceptCode = 
+    "HTTP/1.1 101 Switching Protocols\r\n" +
+    "Upgrade: websocket\r\n" +
+    "Connection: Upgrade\r\n" +
+    "Sec-WebSocket-Accept: " + acceptCode + "\r\n" 
+    + "\r\n"
+
+  let getKey key arr = 
+      try 
+        let item = (Array.find (fun (s:String) -> s.StartsWith(key)) arr)
+        item.Substring key.Length
+      with
+        | _ -> ""
+
+  let getOpCodeFromByte (x : byte) : Option<OpCode> = 
+    if Enum.IsDefined (typeof<OpCode>, x) then Some (LanguagePrimitives.EnumOfValue x) else None
+
+  let makeFrameShortTxt (P:byte array) =
+    let message = new MemoryStream()
+    try                         
+      message.WriteByte(byte 0x81)                              // see 5.6 & 11.8 in [1].  See [2], and [3]
+      message.WriteByte(byte P.Length)                          // assume length < 127 bytes.  as yet, no implementation of masking, extended payload length, or the mask
+      message.Write(P,0,P.Length)                               // TODO: cobine with [8]
+      message.ToArray()
+    finally
+      message.Close()
+
+  let makeFramePing =
+    let message = new MemoryStream()
+    try                         
+      message.WriteByte(byte 0x89)                              
+      message.WriteByte(byte 0)                                 
+      message.ToArray()
+    finally
+      message.Close()
---
+++ FWebSocket/WebSocketServer.fs
@@ -0,0 +1,148 @@
+﻿namespace WebSocket
+
+open System
+open System.IO
+open System.Net
+open System.Net.Sockets
+open System.Text
+open System.Threading
+open System.Runtime.Serialization
+ 
+module WebSocketServer =  
+ 
+  let startMailboxProcessor ct f = MailboxProcessor.Start(f, cancellationToken = ct)
+ 
+  let ping (ctrl : MailboxProcessor<Msg>) interval = async {
+      let pingData = ServerUtil.makeFramePing
+      while true do
+          do! Async.Sleep interval
+          ctrl.Post(Data pingData)            
+      }
+
+  let getTextByMask (mask: byte[]) = 
+      Seq.mapi (fun i x -> x ^^^ mask.[i % mask.Length]) >> Seq.toArray >> System.Text.Encoding.ASCII.GetString
+
+  let isInvert x = (0x80uy &&& x) = 0x80uy
+
+  let getMask (bytes: byte[]): Option<byte array> = 
+      if isInvert bytes.[1] then Some bytes.[2..5] else None
+  
+  let getText (bytes: byte[]) = 
+      match getMask bytes with
+      | Some mask -> 
+          let lastIndex = int ((0x7Fuy &&& bytes.[1]) + 5uy)
+          bytes.[6..lastIndex] |> getTextByMask mask
+      | None -> 
+          let lastIndex = int ((0x7Fuy &&& bytes.[1]) + 2uy)
+          bytes.[2..lastIndex] |> System.Text.Encoding.ASCII.GetString
+
+  let getData (ns : NetworkStream) size = async {
+      while true do
+          let bytes = Array.create size (byte 0)
+          let bytesReadCount = ns.Read (bytes, 0, bytes.Length)
+          let opcodeByte = 0x0Fuy &&& bytes.[0]
+          
+          match ServerUtil.getOpCodeFromByte opcodeByte with
+          | Some opcode -> 
+              match opcode with
+              | OpCode.Pong -> ()
+              | OpCode.Text -> printf "Data: %s" (getText bytes)
+          | None -> ()
+      }
+ 
+  let runController (ct : CancellationToken) = 
+      startMailboxProcessor ct (fun (inbox : MailboxProcessor<Msg>) ->
+          let listeners = new ResizeArray<_>()
+          async {
+              while not ct.IsCancellationRequested do
+                  let! msg = inbox.Receive()
+                  match msg with
+                  | Connect l -> 
+                      Console.WriteLine "Connect"
+                      listeners.Add(l)
+                  | Disconnect l -> 
+                      Console.WriteLine "Disconnect"
+                      listeners.Remove(l) |> ignore
+                  | Data data -> listeners.ForEach(fun l -> l.Post data)
+          }
+      )
+
+  let runWorker (tcp : TcpClient) (ctrl : MailboxProcessor<Msg>) ct = 
+      
+      ignore <| startMailboxProcessor ct (fun (inbox : MailboxProcessor<byte array>) ->
+          let rec handshake = async {            
+              let ns = tcp.GetStream()
+
+              let bytes = Array.create tcp.ReceiveBufferSize (byte 0)
+              let bytesReadCount = ns.Read (bytes, 0, bytes.Length)
+
+              Console.WriteLine("-------------------------------");
+              let result = System.Text.Encoding.UTF8.GetString(bytes);
+              Console.WriteLine(result);
+              Console.WriteLine("-------------------------------");
+
+              if bytesReadCount > 8 then                
+                  let lines = bytes.[..(bytesReadCount-9)]
+                              |> System.Text.UTF8Encoding.UTF8.GetString 
+                              |> fun hs->hs.Split([|"\r\n"|], StringSplitOptions.RemoveEmptyEntries) 
+                  match ServerUtil.isWebSocketsUpgrade lines with
+                  | true ->
+                      let acceptStr =  
+                                      (ServerUtil.getKey "Sec-WebSocket-Key:" lines                 
+                                      ).Substring(1)                                
+                                      |> ServerUtil.calcWSAccept6455 
+                                      |> ServerUtil.createAcceptString6455
+                      Console.WriteLine acceptStr                                   
+                      do! ns.AsyncWrite <| Encoding.ASCII.GetBytes acceptStr
+                      return! run ns tcp.ReceiveBufferSize
+                  | _ ->                      
+                      tcp.Close()                                                   
+              else
+                  tcp.Close()                                                       
+              }
+          and run (ns : NetworkStream) size = async {
+              ctrl.Post(Connect inbox)
+              Async.Start (getData ns size, ct)
+
+              try
+                  while not ct.IsCancellationRequested do
+                      let! time = inbox.Receive()
+                      let df = ServerUtil.makeFramePing
+                      ns.Write(df,0,df.Length)
+
+              finally
+                  ns.Close()
+                  ctrl.Post(Disconnect inbox)
+          }
+
+          handshake
+     ) 
+
+ 
+  let runRequestDispatcher (ipAddress, port) =
+      let listener = new TcpListener(IPAddress.Parse(ipAddress), port)
+      let cts = new CancellationTokenSource()
+      let token = cts.Token
+ 
+      let controller = runController token
+      Async.Start (ping controller ServerUtil.pingInterval, token)
+ 
+      let main = async {
+          try
+              listener.Start()
+              while not cts.IsCancellationRequested do
+                  let! client = Async.FromBeginEnd(listener.BeginAcceptTcpClient, listener.EndAcceptTcpClient)
+                  runWorker client controller token
+          finally
+              listener.Stop()       
+      }
+ 
+      Async.Start(main, token)
+ 
+      { new IDisposable with member x.Dispose() = cts.Cancel()}
+
+  let start (ipAddress, port) = 
+    let dispose = runRequestDispatcher (ipAddress, port)
+    printfn "press any key to stop..."
+    Console.ReadKey() |> ignore
+    dispose.Dispose()
